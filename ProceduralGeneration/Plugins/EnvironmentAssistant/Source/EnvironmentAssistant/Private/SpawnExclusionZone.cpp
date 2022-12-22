934b5e12c52485fd6ca4017408ebc3790df1831f
Kog <kyle.gian@hotmail.com>
2022-12-22 21:39:50 +1100

    created exclusion zone

---
+++ ProceduralGeneration/Plugins/EnvironmentAssistant/Source/EnvironmentAssistant/Private/SpawnExclusionZone.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnExclusionZone.h"
+
+SpawnExclusionZone::SpawnExclusionZone()
+{
+	NoSpawnArea = Create
+}
+
+SpawnExclusionZone::~SpawnExclusionZone()
+{
+}
